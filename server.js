a0650475a611a459bdbc8b170c854d1968ecd46d
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-04 18:49:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 #!/usr/bin/env node
 'use strict';
 const express = require('express');
-const request = require('request');
+const { request } = require('undici');
 const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const compress = require('./src/compress');
 const shouldCompress = require('./src/shouldCompress');
+const redirect = require('./src/redirect'); 
 const bypass = require('./src/bypass');
-const redirect = require('./src/redirect');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', authenticate, params, (req, res) => {
+app.get('/', authenticate, params, async (req, res) => {
     const url = req.params.url;
 
     // Fetch the image from the URL
-    request.get({ url, encoding: null }, (err, origin, buffer) => {
-        if (err || origin.statusCode >= 400) {
-            // Use the redirect function if there is an error
-            return redirect(req, res);
-        }
+    const { statusCode, headers, body } = await request(url);
+    const buffer = await body.arrayBuffer();
 
-        req.params.originType = origin.headers['content-type'] || '';
-        req.params.originSize = buffer.length;
+    if (statusCode >= 400) {
+        return res.status(500).send('Error fetching the image.');
+    }
 
-        if (shouldCompress(req)) {
-            compress(req, res, buffer);
-        } else {
-            // Bypass compression and directly send the original image
-            bypass(req, res, buffer);
-        }
-    });
+    if (statusCode >= 300 && headers.location) {
+        req.params.url = headers.location;
+        return redirect(req, res);
+    }
+
+    req.params.originType = headers['content-type'] || '';
+    req.params.originSize = buffer.byteLength;
+
+    if (shouldCompress(req)) {
+        compress(req, res, Buffer.from(buffer));
+    } else {
+        bypass(req, res, Buffer.from(buffer));
+    }
 });
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
